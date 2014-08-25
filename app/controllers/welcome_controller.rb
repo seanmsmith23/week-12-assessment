3003479d86a3e613dd53d6a010f5957c3383863e
Sean Smith <seansmith1020@gmail.com>
2014-08-25 09:57:10 -0600

    movies appear ordered by their year

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-
+    @movies = Movie.all.order(:year).reverse
   end
 end
--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -7,6 +7,43 @@ feature "Homepage" do
     expect(page).to have_content("Cageflix")
     expect(page).to have_content("Netflix for Nic Cage movies")
   end
+
+  scenario "Movies should appear ordered by their year" do
+    visit "/"
+    click_link("Add Movie")
+
+    fill_in "movie[name]", with: "Some Movie"
+    fill_in "movie[year]", with: "1997"
+    fill_in "movie[synopsis]", with: "Nick Cage acts crazy"
+    click_button "Create Movie"
+
+    click_link("Add Movie")
+
+    fill_in "movie[name]", with: "Another Movie"
+    fill_in "movie[year]", with: "2001"
+    fill_in "movie[synopsis]", with: "Nick Cage acts even crazier"
+    click_button "Create Movie"
+
+    click_link("Add Movie")
+
+    fill_in "movie[name]", with: "Yet Another Movie"
+    fill_in "movie[year]", with: "1999"
+    fill_in "movie[synopsis]", with: "Nick Cage acts even nuttier"
+    click_button "Create Movie"
+
+    within('#0') do
+      expect(page).to have_content("2001")
+    end
+
+    within('#1') do
+      expect(page).to have_content("1999")
+    end
+
+    within('#2') do
+      expect(page).to have_content("1997")
+    end
+
+  end
 end
 
 feature "Add Movies" do
